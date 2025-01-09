59b7c21178756eba3c1568c52b2addcf8ffa56b9
SM2111 <dinoraj.pattabiraman@solverminds.sg>
2025-01-09 14:44:56 +0530

    delete unwanted services

--- libs/libraries/services/src/index.ts
+++ libs/libraries/services/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/services.module';
 
 export * from './lib/storage/storage.service';
-export * from './lib/loader/loader.service';
-export * from './lib/data-sharing/data.service';
 export * from './lib/api/api.service';
--- libs/libraries/services/src/lib/services.module.ts
+++ libs/libraries/services/src/lib/services.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StorageService } from './storage/storage.service';
-import { LoaderService } from './loader/loader.service';
-import { DataService } from './data-sharing/data.service';
 import { ApiService } from './api/api.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FullScreenService } from './full-screen/full-screen.service';
 
 @NgModule({
   imports: [CommonModule,HttpClientModule],
   providers:[
     StorageService,
-    LoaderService,
-    DataService,
     ApiService,
-    FullScreenService,
   ]
 })
 export class ServicesModule {}
