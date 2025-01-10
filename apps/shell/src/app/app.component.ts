eb7a7578148195ed640609ec86edc46954fc8856
SM2111 <dinoraj.pattabiraman@solverminds.sg>
2025-01-10 17:53:09 +0530

    initial commit

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nx-angular';
+
+  ngOnInit() {
+    const text = 'Angular';
+    const reverse = this.reverseString(text);
+    console.log(reverse);
+  }
+
+  reverseString(text: string) {
+    let reversed: string = '';
+    for (let i = 0; i < text.length; i++) {
+      reversed += text[text.length -i - 1];
+    }
+    return reversed;
+  }
 }
